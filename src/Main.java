5a63941c9ffb6a5f92d38287c8730d71ceee5deb
Miroslav Tsadkin <Luckymirik@mail.ru>
2024-03-27 12:55:18 +0300

    Finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
 
+public class Main extends JFrame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Окно");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 200);
+        frame.setLocationRelativeTo(null);
+        JLabel label = new JLabel("Changing",SwingConstants.CENTER);
+        label.setFont(new Font("Vladimir script",Font.BOLD,50));
+        JSlider slider = new JSlider( 5, 100, 50);
+        slider.setMajorTickSpacing(5);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        slider.addChangeListener(e -> {
+            label.setFont(new Font("Vladimir script", Font.BOLD, slider.getValue()));
+        });
+        frame.add(label);
+        frame.add(slider,BorderLayout.SOUTH);
+        frame.setVisible(true);
     }
 }
